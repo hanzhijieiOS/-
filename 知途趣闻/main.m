241e47e1f9c3c67cf3f66891a9633b4699fb32f3
hanzhijie <hanzhijie@xiyou3g.com>
2017-10-25 16:56:38 +0800

    Initial Commit

---
+++ 知途趣闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  知途趣闻
+//
+//  Created by Jay on 2017/10/25.
+//  Copyright © 2017年 hanzhijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
